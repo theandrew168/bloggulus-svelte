f7f9aec12218b11d6470b6a7f0947caf73e1dd42
theandrew168 <andrew@shallowbrooksoftware.com>
2023-12-30 21:27:52 -0600

    Update idle_timeout and max_lifetime

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -3,6 +3,8 @@ import postgres from "postgres";
 const DEFAULT_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres";
 
 const sql = postgres(process.env.DATABASE_URL || DEFAULT_DATABASE_URL, {
+	idle_timeout: 20,
+	max_lifetime: 60 * 30,
 	transform: postgres.camel,
 	onnotice: () => {},
 });
