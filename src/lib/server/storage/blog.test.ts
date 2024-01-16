4e0e47bb796795bc9191ab99d8e04165d88136b5
theandrew168 <andrew@shallowbrooksoftware.com>
2024-01-15 23:29:01 -0600

    Add more blog storage tests

--- src/lib/server/storage/blog.test.ts
+++ src/lib/server/storage/blog.test.ts
@@ -1,7 +1,16 @@
 import { expect, test } from "vitest";
 import { faker } from "@faker-js/faker";
 
-import { createBlog, type CreateBlogParams } from "./blog";
+import {
+	createBlog,
+	listBlogs,
+	type CreateBlogParams,
+	readBlogById,
+	readBlogByFeedUrl,
+	deleteBlogById,
+	updateBlog,
+	updateBlogSyncedAt,
+} from "./blog";
 import { isValidUuid } from "$lib/utils";
 
 function generateFakeBlog(): CreateBlogParams {
@@ -21,4 +30,60 @@ test("createBlog", async () => {
 	const blog = await createBlog(params);
 	expect(isValidUuid(blog.id)).toEqual(true);
 	expect(blog.feedUrl).toEqual(params.feedUrl);
+});
+
+test("listBlogs", async () => {
+	const blogs = await Promise.all([
+		createBlog(generateFakeBlog()),
+		createBlog(generateFakeBlog()),
+		createBlog(generateFakeBlog()),
+	]);
+
+	const got = await listBlogs();
+	const ids = got.map((blog) => blog.id);
+	for (const blog of blogs) {
+		expect(ids.includes(blog.id)).toEqual(true);
+	}
+});
+
+test("readBlogById", async () => {
+	const params = generateFakeBlog();
+	const blog = await createBlog(params);
+	const got = await readBlogById(blog.id);
+	expect(got?.id).toEqual(blog.id);
+});
+
+test("readBlogByFeedUrl", async () => {
+	const params = generateFakeBlog();
+	const blog = await createBlog(params);
+	const got = await readBlogByFeedUrl(blog.feedUrl);
+	expect(got?.id).toEqual(blog.id);
+});
+
+test("updateBlog", async () => {
+	const params = generateFakeBlog();
+	const blog = await createBlog(params);
+	await updateBlog(blog.id, "foo", "bar");
+	const got = await readBlogById(blog.id);
+	expect(got?.id).toEqual(blog.id);
+	expect(got?.etag).toEqual("foo");
+	expect(got?.lastModified).toEqual("bar");
+});
+
+test("updateBlogSyncedAt", async () => {
+	const params = generateFakeBlog();
+	const blog = await createBlog(params);
+	const syncedAt = faker.date.past();
+	await updateBlogSyncedAt(blog.id, syncedAt);
+	const got = await readBlogById(blog.id);
+	expect(got?.id).toEqual(blog.id);
+	expect(got?.syncedAt).toEqual(syncedAt);
+});
+
+test("deleteBlogById", async () => {
+	const params = generateFakeBlog();
+	const blog = await createBlog(params);
+	await deleteBlogById(blog.id);
+	const got = await readBlogById(blog.id);
+	expect(got).toBeNull;
 });
