3c0572d736c2e3d97fa36a56bcbe6b9ec951f254
theandrew168 <andrew@shallowbrooksoftware.com>
2024-03-25 00:21:29 -0500

    Add some comments explaining how the tx works

--- src/lib/server/storage/storage.ts
+++ src/lib/server/storage/storage.ts
@@ -26,11 +26,20 @@ export class Storage {
 
 	async transaction(operation: Operation) {
 		try {
+			// Calling begin() will start a new database transaction and automatically
+			// roll it back if any errors are thrown from the given lambda.
 			await this.sql.begin(async (tx) => {
+				// Create a new instance of the Storage class using this transaction.
 				const storage = new Storage(tx);
+
+				// Use the transaction-based Storage class for this atomic operation.
+				// If an error occurs within this operation, the transaction will
+				// be rolled back.
 				await operation(storage);
 			});
 		} catch (e) {
+			// Check for our special sentinel error and prevent it from propagating
+			// if found. All other errors should continue to bubble up.
 			const isRollbackError = e instanceof RollbackError;
 			if (!isRollbackError) {
 				throw e;
