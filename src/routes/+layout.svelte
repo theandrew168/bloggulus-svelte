805b067279c34d052ca6ffb61c4c028012d48552
theandrew168 <andrew@shallowbrooksoftware.com>
2025-05-29 22:54:40 -0500

    Remove old Karla font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-	import "$lib/styles/reset.css";
-	import "$lib/styles/fonts.css";
-	import "$lib/styles/style.css";
 	import Header from "$lib/components/Header.svelte";
 	import Footer from "$lib/components/Footer.svelte";
 </script>
