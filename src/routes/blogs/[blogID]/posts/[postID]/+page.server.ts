6d55245f59823f81208f21f9484571dfdfbfb483
theandrew168 <andrew@shallowbrooksoftware.com>
2025-06-19 00:33:36 -0500

    Add post details page for admins

---
+++ src/routes/blogs/[blogID]/posts/[postID]/+page.server.ts
@@ -0,0 +1,51 @@
+import { redirect } from "@sveltejs/kit";
+
+import { isValidUUID } from "$lib/server/utils";
+import { errorBadRequest, errorNotFound, errorUnauthorized } from "$lib/server/web/errors";
+
+import type { Actions, PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async ({ locals, params }) => {
+	const postID = params.postID;
+	if (!isValidUUID(postID)) {
+		errorNotFound();
+	}
+
+	const account = locals.account;
+	if (!account) {
+		errorNotFound();
+	}
+
+	if (!account.isAdmin) {
+		errorUnauthorized();
+	}
+
+	const post = await locals.query.readPostDetailsByID(postID);
+	if (!post) {
+		errorNotFound();
+	}
+
+	return { post };
+};
+
+export const actions = {
+	default: async ({ locals, request, params }) => {
+		if (!locals.account) {
+			errorNotFound();
+		}
+
+		const data = await request.formData();
+		const postID = data.get("postID")?.toString();
+		if (!postID) {
+			errorBadRequest();
+		}
+
+		if (!isValidUUID(postID)) {
+			errorBadRequest();
+		}
+
+		await locals.command.post.deletePost(postID);
+
+		redirect(303, `/blogs/${params.blogID}`);
+	},
+} satisfies Actions;
---
+++ src/routes/blogs/[blogID]/posts/[postID]/+page.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+	import { enhance } from "$app/forms";
+
+	import Button from "$lib/components/Button.svelte";
+	import Input from "$lib/components/Input.svelte";
+
+	import type { PageProps } from "./$types";
+
+	let { data }: PageProps = $props();
+</script>
+
+<section>
+	<article class="links">
+		<h1>{data.post.title}</h1>
+		<ul>
+			<li><a href={data.post.url}>(URL)</a></li>
+			<li><a href={`/blogs/${data.post.blogID}`}>(Blog)</a></li>
+		</ul>
+	</article>
+	<article class="published">
+		<h2>Published at:</h2>
+		<time datetime={data.post.publishedAt.toISOString()}>{data.post.publishedAt.toLocaleString()}</time>
+	</article>
+	<article class="actions">
+		<h2>Actions</h2>
+		<form method="POST" use:enhance>
+			<Input type="hidden" name="postID" value={data.post.id} />
+			<Button>Delete</Button>
+		</form>
+	</article>
+</section>
+
+<style>
+	section {
+		max-width: var(--container-width);
+		margin: 0 auto;
+		padding: 1em;
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
+	}
+
+	.links h1 {
+		font-size: 2rem;
+		margin-bottom: 0.5em;
+	}
+
+	.links ul {
+		display: flex;
+		gap: 0.5em;
+	}
+
+	.links a {
+		color: var(--color-dark);
+		text-decoration: none;
+	}
+
+	.links a:hover {
+		cursor: pointer;
+		text-decoration: underline;
+	}
+
+	.published h2 {
+		font-size: 1.5rem;
+		margin-bottom: 0.5em;
+	}
+
+	.actions h2 {
+		font-size: 1.5rem;
+		margin-bottom: 0.5em;
+	}
+</style>
