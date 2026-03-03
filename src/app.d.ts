54adea3ef283ee5bf40f315201013b5cbb857d16
theandrew168 <andrew@shallowbrooksoftware.com>
2026-03-02 20:21:58 -0600

    Fix how Option and Result types are imported

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,7 +1,8 @@
+import type { Option as OptionType, Result as ResultType } from "$lib/globals";
 import type { Command } from "$lib/server/command";
 import type { Config } from "$lib/server/config";
 import type { WebQuery } from "$lib/server/query/web";
-import type { Account, Option as OptionType, Result as ResultType } from "$lib/types";
+import type { Account } from "$lib/types";
 
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
